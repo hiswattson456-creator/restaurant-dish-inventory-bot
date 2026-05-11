816ccd9f51db106613d63720b75972499772819a
Wattson <hiswattson456@gmail.com>
2026-05-12 00:20:45 +0900

    Delete ресторан_заказчику directory

--- ресторан_заказчику/src/bot/commands/start.js
+++
@@ -1,8 +0,0 @@
-/**
- * Обработчик команды /start — переходит в каталог складов.
- */
-async function startCommand(ctx) {
-  await ctx.scene.enter('catalog_warehouse');
-}
-
-module.exports = startCommand;
